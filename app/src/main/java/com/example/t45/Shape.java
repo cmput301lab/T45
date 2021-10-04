4d22c57199ca306cb865addf2200b19d4a9dd9d0
Hameem Bhuiyan <hbhuiyan@ualberta.ca>
2021-10-04 13:53:33 -0600

    Update Shape.java

--- app/src/main/java/com/example/t45/Shape.java
+++ app/src/main/java/com/example/t45/Shape.java
@@ -5,5 +5,5 @@ public abstract class Shape {
     private int y;
     String color = "RED";
     String color_2 = "WHITE";
-    String color_3 = "$PUT_YOUR_CHOICE_OF_COLOR";
+    String color_3 = "Green";
 }
